1e4e4d4202aadbfb8e02aba6ea63e92c6c21380f
Paweł Mikłas <pablomiklas@gmail.com>
2021-11-09 21:23:41 +0100

    Added index for Book model

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -37,25 +37,26 @@ namespace LibApp.Controllers
             return Content("id=" + BookId);
         }
 
-        public IActionResult Index(int? pageIndex, string sortBy)
+        public IActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+            var books = GetBooks();
 
-            if (String.IsNullOrEmpty(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            return View(books);
         }
 
         [Route("books/released/{year:regex(^\\d{{4}}$):min(1925):max(2021)}/{month:range(1,12)}")]
         public IActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book { Id = 1, Name = "Hamlet"},
+                new Book { Id = 2, Name = "Ulysses"},
+            };
         }
     }
 }

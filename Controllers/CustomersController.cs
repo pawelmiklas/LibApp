8d07ae098a474595e57a4def563222a117498f3d
Mikłas Paweł <pawel.miklas@microsoft.wsei.edu.pl>
2021-11-14 12:01:01 +0100

    Added Eager Loading to CustomerController Index

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,5 +1,7 @@
-﻿using LibApp.Models;
+﻿using LibApp.Data;
+using LibApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +11,23 @@ namespace LibApp.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public IActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -26,15 +35,6 @@ namespace LibApp.Controllers
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Jan Kowalski"},
-                new Customer { Id = 1, Name = "Andrzej Nowak"}
-            };
         }
     }
 }

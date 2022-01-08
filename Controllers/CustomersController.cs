339134abcb9086ba0b7d1506704994d301603328
Paweł Mikłas <pablomiklas@gmail.com>
2022-01-08 13:49:44 +0100

    Added anti-forgery token to CustomerForm

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -69,6 +69,7 @@ namespace LibApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)

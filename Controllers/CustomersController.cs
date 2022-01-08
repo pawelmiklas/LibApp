d57effd6c1066a2342f8920833e302887d5be12a
Paweł Mikłas <pablomiklas@gmail.com>
2022-01-08 13:44:25 +0100

    Added server-side validation for Customer model

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -44,6 +44,7 @@ namespace LibApp.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -70,6 +71,18 @@ namespace LibApp.Controllers
         [HttpPost]
         public IActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,14 +9,16 @@ namespace LibApp.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please provide customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool HasNewsletterSubscribed { get; set; }
         [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
+        [Required(ErrorMessage = "Membership Type is required")]
         public byte MembershipTypeId { get; set; }
         [Display(Name="Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
     }
 }

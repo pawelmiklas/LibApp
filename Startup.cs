9c69af00174fccd27ad3811bba452e3e043fb304
Paweł Mikłas <pablomiklas@gmail.com>
2022-01-21 23:28:07 +0100

    Returned to JSON response output format

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,11 +36,7 @@ namespace LibApp
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews(options =>
-            {
-                options.OutputFormatters.RemoveType(typeof(SystemTextJsonOutputFormatter));
-                options.ReturnHttpNotAcceptable = true;
-            }).AddXmlSerializerFormatters();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

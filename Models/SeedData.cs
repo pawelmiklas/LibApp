0be53f7faeede43793d5e454df7130cea817f02a
Paweł Mikłas <pablomiklas@gmail.com>
2022-02-21 23:15:04 +0100

    1

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -55,6 +55,88 @@ namespace LibApp.Models
                         DiscountRate = 20,
                     }
                  );
+
+                if (!context.Genre.Any())
+                {
+                    context.Genre.AddRange(
+                        new Genre
+                        {
+                            Id = 1,
+                            Name = "Fiction"
+                        },
+                        new Genre
+                        {
+                            Id = 2,
+                            Name = "Science fiction"
+                        },
+                        new Genre
+                        {
+                            Id = 3,
+                            Name = "Novel"
+                        }
+                    );
+                }
+
+                if (!context.Customers.Any())
+                {
+                    context.Customers.AddRange(
+                        new Customer
+                        {
+                            Name = "Tom",
+                            HasNewsletterSubscribed = false,
+                            MembershipTypeId = 2,
+                            Birthdate = DateTime.Now.AddYears(-20)
+                        },
+                        new Customer
+                        {
+                            Name = "Mustafa",
+                            HasNewsletterSubscribed = true,
+                            MembershipTypeId = 1,
+                            Birthdate = DateTime.Now.AddYears(-40)
+                        },
+                        new Customer
+                        {
+                            Name = "Jade",
+                            HasNewsletterSubscribed = false,
+                            MembershipTypeId = 2,
+                            Birthdate = DateTime.Now.AddYears(-30)
+                        }
+                    );
+                }
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book
+                        {
+                            Name = "It ends with us",
+                            AuthorName = "J. K. Rowling",
+                            GenreId = 1,
+                            DateAdded = DateTime.Now.AddDays(-100),
+                            ReleaseDate = DateTime.Now.AddDays(-200),
+                            NumberInStock = 70
+                        },
+                        new Book
+                        {
+                            Name = "No Longer Human",
+                            AuthorName = "Lucy Maud Montgomery",
+                            GenreId = 2,
+                            DateAdded = DateTime.Now.AddDays(-10),
+                            ReleaseDate = DateTime.Now.AddDays(-150),
+                            NumberInStock = 10
+                        },
+                        new Book
+                        {
+                            Name = "Seven Husbands of Evelyn Hugo",
+                            AuthorName = "Scott Fitzgerald",
+                            GenreId = 1,
+                            DateAdded = DateTime.Now.AddDays(-50),
+                            ReleaseDate = DateTime.Now.AddDays(-80),
+                            NumberInStock = 500
+                        }
+                        );
+                }
+
                 context.SaveChanges();
             }
         }
